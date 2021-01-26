9b985b6550d447a1780c0707f1313ed96de255e1
Amybunny <47083737+Amybunny@users.noreply.github.com>
2021-01-26 11:58:13 +0900

    Update Main.java

--- src/main/java/servlet/Main.java
+++ src/main/java/servlet/Main.java
@@ -16,7 +16,7 @@ import model.EVLogic;
 import model.Sentence;
 
 
-@WebServlet("/")
+@WebServlet("/Main")
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
